da2d1556421627ecf2fd5adbfa3b43c1a9690be1
Caio Miranda <caiomirand@gmail.com>
2020-05-31 14:05:00 -0300

    Init commit

---
+++ src/utils/geoCode.js
@@ -0,0 +1,22 @@
+const request = require('request');
+
+const geoCode = (address, callback) => {
+    const geoUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) +  '.json?access_token=pk.eyJ1IjoiY2FpdXppbm4iLCJhIjoiY2s5Mnl3YnlrMDAzaDNscWp5bDh0aHYzMyJ9.s8rjy4WEh0mBTxE41jFO9Q';
+    
+    request ({ url: geoUrl, json: true}, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to location services!', undefined)
+        } else if (body.features.length === 0) {
+            callback('Unable to find location. Try another search', undefined);
+        } else {
+            callback(undefined, {
+                latitude: body.features[0].center[1],
+                longitude: body.features[0].center[0],
+                location: body.features[0].place_name,
+                text: body.features[0].text
+            })
+        }
+    })
+}
+
+module.exports = geoCode;
